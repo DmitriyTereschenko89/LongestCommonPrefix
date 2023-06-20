0ca8f0205f8b73d138bd021293a3289d0f969d06
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-20 11:19:24 +0300

    Добавьте файлы проекта.

---
+++ CommonPrefixLongest.cs
@@ -0,0 +1,79 @@
+﻿namespace LongestCommonPrefix
+{
+	internal class CommonPrefixLongest
+	{
+		private class TrieNode
+		{
+			public bool isWord;
+			public readonly TrieNode[] children;
+
+			public TrieNode()
+			{
+				children = new TrieNode[26];
+			}
+		}
+
+		private class Trie
+		{
+			private TrieNode root;
+
+			public Trie()
+			{
+				root = new();
+			}
+
+			public void Add(string str)
+			{
+				TrieNode node = root;
+				foreach (char ch in str)
+				{
+					if (node.children[ch - 'a'] is null)
+					{
+						node.children[ch - 'a'] = new TrieNode();
+					}
+					node = node.children[ch - 'a'];
+				}
+				node.isWord = true;
+			}
+
+			public int GetCommonPrefix(string str)
+			{
+				TrieNode node = root;
+				for (int i = 0; i < str.Length; ++i)
+				{
+					if (node.children[str[i] - 'a'] is null)
+					{
+						return i;
+					}
+					node = node.children[str[i] - 'a'];
+				}
+				return str.Length;
+			}
+		}
+
+		public string LongestCommonPrefix(string[] strs)
+		{
+			int n = strs.Length;
+			int smallestIndex = 0;
+			for (int i = 0; i < n; ++i)
+			{
+				if (strs[i].Length < strs[smallestIndex].Length)
+				{
+					smallestIndex = i;
+				}
+			}
+			Trie trie = new();
+			trie.Add(strs[smallestIndex]);
+			int maxLength = int.MaxValue;
+			for (int i = 0; i < n; ++i)
+			{
+				if (i == smallestIndex)
+				{
+					maxLength = Math.Min(maxLength, strs[i].Length + 1);
+				}
+				maxLength = Math.Min(maxLength, trie.GetCommonPrefix(strs[i]) + 1);
+			}
+			return maxLength == 0 ? string.Empty : strs[smallestIndex][..(maxLength - 1)];
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LongestCommonPrefix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CommonPrefixLongest commonPrefixLongest = new();
+			Console.WriteLine(commonPrefixLongest.LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+			Console.WriteLine(commonPrefixLongest.LongestCommonPrefix(new string[] { "dog", "racecar", "car" }));
+			Console.WriteLine(commonPrefixLongest.LongestCommonPrefix(new string[] { "" }));
+			Console.WriteLine(commonPrefixLongest.LongestCommonPrefix(new string[] { "a" }));
+			Console.WriteLine(commonPrefixLongest.LongestCommonPrefix(new string[] { "reflower", "flow", "flight" }));
+        }
+	}
+}
